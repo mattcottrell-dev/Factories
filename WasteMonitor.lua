4106af4f4f0e1f09086ec4c56c20986f8b81723a
mattcottrell-dev <72380632+mattcottrell-dev@users.noreply.github.com>
2024-11-02 10:39:01 +0000

    Added Trend

--- WasteMonitor.lua
+++ WasteMonitor.lua
@@ -1,6 +1,9 @@
-_display = "46F52A304CF0A1E868F457BC10272802"  -- ID of the Large Screen
+-- Variables
+_display = "7A9BCB6F4AF64114F5E2F99271C10643"  -- ID of the Large Screen
 _screenWidth = 70                              -- Adjustable width of Large Screen
 _screenHeight = 33                
+
+_maxCounter = 60
 
 -- Factory Variables
 _factory = "F_PFR"
@@ -17,16 +20,54 @@ _plutoniumFuelRod = "I_PFR"
 wasteAmount = 0
 nfuAmount = 0
 plpAmount = 0
+epcAmount = 0
+
+wasteTrend = "stable    "
+nfuTrend = "stable    "
+plpTrend = "stable    "
+epcTrend = "stable    "
 
 _containerCount = 0
 _machineNFUCount = 0
 _machinePLPCount = 0
 
 -- Limits
-_maxWaste = 20
-_maxNFU = 20
-_maxPLP = 20
-_maxEPC = 20
+_maxWaste = 200
+_maxNFU = 500
+_maxPLP = 200
+_maxEPC = 200
+
+
+-- Functions
+local function addEntry(inTable, inEntry)
+    -- Add the new entry to the table
+
+    table.insert(inTable, inEntry)
+    
+    -- Keep only the most recent 5 entries
+    if #inTable > 5 then
+        table.remove(inTable, 1)  -- Remove the oldest entry
+    end
+end
+
+local function getTrend(values)
+    local trends = {}
+    for i = 2, #values do
+        trends[i-1] = values[i] - values[i-1]
+    end
+    
+    local trendType = "stable    "
+    for _, change in ipairs(trends) do
+        if change > 0 then
+            trendType = "increasing"
+        elseif change < 0 then
+            trendType = "decreasing"
+            break
+        end
+    end    
+    return trendType
+end
+
 
 function sortingFunction(machine1, machine2)
     return machine1.Nick < machine2.Nick
@@ -37,7 +78,6 @@ function round(num, numDecimalPlaces, numPlaces)
 end
 
 function getContainerAmount(container)
-
     containerAmount = 0
     containerInv = container:getInventories()
     for i,inventory in ipairs(containerInv) do
@@ -73,6 +113,13 @@ end
 
 
 
+
+
+
+
+
+
+
 -- Get containers
 containerIDs = component.proxy(component.findComponent(_containers))
 containers = {}
@@ -80,15 +127,6 @@ for i, container in ipairs(containerIDs) do
     table.insert(containers,container)
     _containerCount = _containerCount + 1
 end
-
--- Get NFU machines --
-machineNFUIDs = component.proxy(component.findComponent(_nonFissleUranium))
-machinesNFU = {}
-for i, machine in ipairs(machineNFUIDs) do
-    table.insert(machinesNFU, machine)
-    _machineNFUCount = _machineNFUCount + 1
-end
-table.sort(machinesNFU, sortingFunction)
 
 for i, container in ipairs(containers) do
     containerNick = container.Nick
@@ -101,36 +139,44 @@ for i, container in ipairs(containers) do
     if string.find(containerNick, _plutoniumPellet) then
         plpContainer = container
     end
+    if string.find(containerNick, _encasedPlutoniumCell) then
+        epcContainer = container        
+    end
+    if string.find(containerNick, _plutoniumFuelRod) then
+        pfrContainer = container        
+    end
 end
 
--- Get Plutonium Pellets machines --
-machinePLPIDs = component.proxy(component.findComponent(_plutoniumPellet))
+
+-- Get Machines
+machineIDs = component.proxy(component.findComponent(_factory))
+machines = {}
+machinesNFU = {}
 machinesPLP = {}
-for i, machine in ipairs(machinePLPIDs) do
-    table.insert(machinesPLP, machine)
-end
-table.sort(machinesPLP, sortingFunction)
-
--- Get Encased Plutonium Cells machines --
-machineEPCIDs = component.proxy(component.findComponent(_encasedPlutoniumCell))
 machinesEPC = {}
-for i, machine in ipairs(machineEPCIDs) do
-    table.insert(machinesEPC, machine)
+machinesPFR = {}
+
+for i, machine in ipairs(machineIDs) do
+    table.insert(machines,machine)
+    --print(machine.Nick)
 end
-table.sort(machinesPLP, sortingFunction)
 
-
--- Get Containers
-for i, container in ipairs(containers) do
-    containerNick = container.Nick
-    if string.find(containerNick, _uraniumWaste) then
-        wasteContainer = container
+for i, machine in ipairs(machines) do
+    machineNick = machine.Nick
+    if string.find(machineNick, _nonFissleUranium) then
+        table.insert(machinesNFU, machine)
     end
-    if string.find(containerNick, _nonFissleUranium) then
-        nfuContainer = container
+    if string.find(machineNick, _plutoniumPellet) then
+        table.insert(machinesPLP, machine)
     end
-    if string.find(containerNick, _plutoniumPellet) then
-        plpContainer = container        
+    if string.find(machineNick, _encasedPlutoniumCell) then
+        table.insert(machinesEPC, machine)
+    end
+    if string.find(machineNick, _encasedPlutoniumCell) then
+        table.insert(machinesEPC, machine)
+    end
+    if string.find(machineNick, _plutoniumFuelRod) then
+        table.insert(machinesPFR, machine)
     end
 end
 
@@ -145,26 +191,55 @@ gpu:bindScreen(screen)
 ClearScreen()
 DrawBackground()
 
+wasteAmounts = {}
+nfuAmounts = {}
+plpAmounts = {}
+epcAmounts = {}
+
+dataCounter = 0
+nfuCounter = 0
+plpCounter = 0
+epcCounter = 0
+
+
 while true do
 
     gpu:setBackground(0, 0, 0, 1)
     gpu:setForeground(1, 1, 1, 1)
 
-    gpu:setText(3, 2 + _screenOffset, "Process | Backlog | Status ")
+    dataCounter = dataCounter + 1
+    nfuCounter = nfuCounter + 1
+    plpCounter = plpCounter + 1
+    epcCounter = epcCounter + 1
 
     wasteAmount = getContainerAmount(wasteContainer)
     nfuAmount = getContainerAmount(nfuContainer)
     plpAmount = getContainerAmount(plpContainer)
+    epcAmount = getContainerAmount(epcContainer)
 
-    print("Uranium Waste: " .. wasteAmount)
-    print("Non-fissle Uranium: " .. nfuAmount)
-    print("Plutonium Pellets: " .. plpAmount)
+    if dataCounter == _maxCounter then
+        addEntry(wasteAmounts,wasteAmount)
+        wasteTrend = getTrend(wasteAmounts)
+
+        addEntry(nfuAmounts,nfuAmount)
+        nfuTrend = getTrend(nfuAmounts)
+
+        addEntry(plpAmounts,plpAmount)
+        plpTrend = getTrend(nfuAmounts)
+
+        addEntry(epcAmounts,epcAmount)
+        epcTrend = getTrend(nfuAmounts)
+
+        dataCounter = 0
+    end
 
     -- Set NFU
     if wasteAmount > _maxWaste then
         nfuProductionBoost = 2
+        nfuStatus = "Slooped"
     else
         nfuProductionBoost = 1
+        nfuStatus = "Normal "
     end
 
     for i,machine in ipairs(machinesNFU) do
@@ -174,12 +249,15 @@ while true do
         end
     end
 
+    
 
     -- Set Plutonium Pellets
     if nfuAmount > _maxNFU then
         plpPotential = 1.5
+        plpStatus = "Overclocked"
     else
         plpPotential = 1
+        plpStatus = "Normal     "
     end
 
     for i,machine in ipairs(machinesPLP) do
@@ -189,21 +267,54 @@ while true do
         end
     end
 
-        -- Set Encased Plutonium Cells
-        if plpAmount > _maxPLP then
-            epcPotential = 1.5
-        else
-            epcPotential = 1
-        end
     
-        for i,machine in ipairs(machinesEPC) do
-            if machine.potential ~= epcPotential and machine.maxPotential >= epcPotential then
-                print("Setting potential for " .. machine.Nick .. " to " .. math.floor(epcPotential * 100) .. "%")
-                machine:setPotential(epcPotential)
-            end
-        end
 
-    event.pull(5)
+    -- Set Encased Plutonium Cells
+    if plpAmount > _maxPLP then
+        epcPotential = 1.5
+        epcStatus = "Overclocked"
+    else
+        epcPotential = 1
+        epcStatus = "Normal     "
+    end
+
+    for i,machine in ipairs(machinesEPC) do
+        if machine.potential ~= epcPotential and machine.maxPotential >= epcPotential then
+            print("Setting potential for " .. machine.Nick .. " to " .. math.floor(epcPotential * 100) .. "%")
+            machine:setPotential(epcPotential)
+        end
+    end
+
+
+
+    -- Set Plutonium Fuel ROds
+    if epcAmount > _maxEPC then
+        pfrPotential = 1.5
+        pfrStatus = "Overclocked"
+    else
+        pfrPotential = 1
+        pfrStatus = "Normal     "
+    end
+
+    for i,machine in ipairs(machinesPFR) do
+        if machine.potential ~= pfrPotential and machine.maxPotential >= pfrPotential then
+            print("Setting potential for " .. machine.Nick .. " to " .. math.floor(pfrPotential * 100) .. "%")
+            machine:setPotential(pfrPotential)
+        end
+    end
+
+    gpu:setText(3, 2 + _screenOffset, "Uranium Waste           : " .. wasteAmount .. " - " .. wasteTrend)
+    gpu:setText(3, 3 + _screenOffset, "Non-fissle Uranium      : " .. nfuAmount .. " - " .. nfuTrend)
+    gpu:setText(3, 4 + _screenOffset, "Plutonium Pellets       : " .. plpAmount .. " - " .. plpTrend)
+    gpu:setText(3, 5 + _screenOffset, "Encased Plutonium Cells : " .. epcAmount .. " - " .. epcTrend)
+
+    gpu:setText(3, 9 + _screenOffset, "Non-Fissle Uranium      : " .. nfuStatus)
+    gpu:setText(3, 10 + _screenOffset, "Plutonium Pellets       : " .. plpStatus)
+    gpu:setText(3, 11 + _screenOffset, "Encased Plutonium Cells : " .. epcStatus)
+    gpu:setText(3, 12 + _screenOffset, "Plutonium Fuel Rods     : " .. pfrStatus)
+
+    gpu:flush()
+    event.pull(1)
 end
 
 
